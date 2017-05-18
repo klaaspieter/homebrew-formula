73b51bfce670a82eca2bd50995643fc65641d1c9
Klaas Pieter Annema <kpa@annema.me>
2017-05-18 14:06:11 +0200

    Deprecate pass-qr

--- pass-qr.rb
+++ pass-qr.rb
@@ -1,12 +1,26 @@
 class PassQr < Formula
   desc "Convenience commands for reading and inserting QRs in pass on macOS"
   homepage "https://github.com/klaaspieter/pass-qr"
-  url "https://github.com/klaaspieter/pass-qr/archive/0.1.tar.gz"
-  sha256 "3898e427c89c794eeca10153e01e7dd8e923d84c466e349f8e7002d461e18a50"
+  url "https://github.com/klaaspieter/pass-qr/archive/v0.1.1.tar.gz"
+  sha256 "a7d85c805169aac5eab3dd91c75a2fe4bc5a95f29860697ab8060d72358d8043"
 
   depends_on "pass"
+  depends_on "qrencode"
+  depends_on "pngpaste"
+  depends_on "zbar"
 
   def install
     bin.install "pass-insert-qr", "pass-show-qr"
+  end
+
+  def caveats
+    opoo <<-EOS.undent
+      pass-qr is deprecated because pass supports showing QRs.
+    EOS
+  end
+
+  test do
+    system "#{bin}/pass-insert-qr"
+    system "#{bin}/pass-show-qr"
   end
 end
