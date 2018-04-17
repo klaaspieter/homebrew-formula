dfc0aa79dd2435edc3c3e57f2994697c41326a3c
Klaas Pieter Annema <kpa@annema.me>
2018-04-17 21:47:48 +0200

    Update sha256 to the version that installs the Makefile and tests
    Makes `brew test chxcode` work.

--- chxcode.rb
+++ chxcode.rb
@@ -2,7 +2,7 @@ class Chxcode < Formula
   desc "Changes the current Xcode"
   homepage "https://github.com/klaaspieter/chxcode"
   url "https://github.com/klaaspieter/chxcode/archive/0.0.1.tar.gz"
-  sha256 "33d9abc44722d7e1747e94834f979a5e63fdddda7a80ad4293dac0618af32ecc"
+  sha256 "af8dc2fe2dc9c567002850bc93642056b5839771b59b332472eaf8c5dd049322"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
