454a0801f70fbf67aa0069494a29bcfb60884c8b
Klaas Pieter Annema <kpa@annema.me>
2018-06-06 15:01:40 +0200

    Add chxcode 0.0.3

--- chxcode.rb
+++ chxcode.rb
@@ -1,8 +1,8 @@
 class Chxcode < Formula
   desc "Changes the current Xcode"
   homepage "https://github.com/klaaspieter/chxcode"
-  url "https://github.com/klaaspieter/chxcode/archive/0.0.2.tar.gz"
-  sha256 "886911a5dcb31819dd55170e0e6a979cba51d79a7b9aa215e8eb062cba69fa29"
+  url "https://github.com/klaaspieter/chxcode/archive/0.0.3.tar.gz"
+  sha256 "bc5be878f9cd6266e50dd77c45ed36509290ce701a7130cdd0bddda432ace75b"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
